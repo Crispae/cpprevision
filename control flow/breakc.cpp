c969ededec261a70df760787158fe265042f977a
unknown <sauravbioinfo3376@gmail.com>
2021-04-11 22:20:51 +0530

    Added break statement

---
+++ control flow/breakc.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+/// contiune and break in c++ is same as python
+
+int main(){
+
+    int n;
+    n = 10;
+    for (int i;i<=n;i++){
+        cout<<"*"<<endl;
+
+        if (i==5){
+            break;
+        }
+    }
+
+    return 0;
+
+
+}
